140560fc1b950a1c0985d4918445d0075d5b1467
AdiFejza <93136623+AdiFejza@users.noreply.github.com>
2022-04-24 00:25:22 +0200

    Update blog1.php

--- blog1.php
+++ blog1.php
@@ -1,3 +1,6 @@
+<?php
+require_once "./controllers/BlogController.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -5,155 +8,65 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>E-commerce website</title>
-
+        <link rel="icon" href="image/websitelogo.png">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
         <link rel="stylesheet" href="css/style1.css">
     </head>
     <body>
-        <header class="header">
-
-            <a href="index.html" class="logo"> <i class=""></i> Skincare </a>
-        
-            <nav class="navbar">
-                <a href="index.html">home</a>
-                <a href="file:///C:/Users/rines/Desktop/Projekti%20web/index.html#:~:text=Read%20More-,Our,Features,-Organic%20Ingredients">features</a>
-                <a href="products.html">products</a>
-                <a href="file:///C:/Users/rines/Desktop/Projekti%20web/index.html#:~:text=Product,Categories">categories</a>
-                <a href="blog1.html" >blogs</a>
-            </nav>
-            
-            <div class="icons">
-                <div class="fas fa-bars" id="menu-btn"></div>
-                <div class="fas fa-search" id="search-btn"></div>
-                <div class="fas fa-shopping-cart" id="cart-btn"></div>
-                <div class="fas fa-user" id="login-btn"></div>
-            </div>
-        
-            <form action="" class="search-form">
-                <input type="search" id="search-box" placeholder="search here...">
-                <label for="search-box" class="fas fa-search"></label>
-            </form>
-        
-            <form action="" class="login-form" onsubmit="return false">
-                <h3>login now</h3>
-                <input type="email" placeholder="your email" id="emailSign" class="box">
-                <input type="password" placeholder="your password" id="passSign" class="box">
-                <p>don't have an account <a href="signup.html">create now</a></p>
-                <input type="submit" value="login now" class="btn" onclick="validateForm()">
-            </form>
-        </header>
+       <!-- header -->
+       <?php include "includes/header.php";?>
 <!------------------------------------------------------------------------------------------->
-
-        <selection class="blog1" id="blog1">
+    <selection class="blog1" id="blog1">
+        <?php
+        
+        $blogs=new BlogController;
+        $all=$blogs->readBlog();
+            echo '
             <div class="main1">
-                <h1>WHAT IS BLUE LIGHT AND HOW DOES IT AFFECT SKIN?</h1>
- <p>
-   
- Sunlight creates all kinds of light, including UV, visible and infrared. Half the sunlight spectrum consists of visible light. 
- The other half is made up of UV and infrared light. The blue/violet band of the visible spectrum is known as High Energy Visible Light or HEV Light. 
- Light is measured in wavelengths. Ultraviolet A (UVA) is made up of wavelengths 320 to 400 nm (nanometers) in length. Ultraviolet B (UVB) wavelengths are 280 to 320 nm. 
- And blue light is made up of wavelengths 380 to 500 nm. These wavelength measurements correlate to how deeply each can penetrate skin.   UVB rays, which have shorter wavelengths, are absorbed by the epidermis, 
- or top layer of skin.  These are the rays responsible for causing sunburn and some forms of skin cancer,
- including malignant melanoma. UVA rays are absorbed into the dermis, or middle layer of skin. UVA rays are responsible for prematurely aging your skin, causing wrinkles, 
- age spots and some cancer formation. Blue light has the longest wavelength and is known to penetrate the hypodermis, or deepest layer of skin. 
- 
-    
-</p>
+            <h2>'.$all[0]['title'].'</h2>
+            <p>'.$all[0]['body'].'</p>
             </div>
-        </selection>
-
+              '
+        ;
+        ?>
+    </selection>
 <!------------------------------------------------------------------------------------------->
 
 <selection class="blog2" id="blog2">
-    <div class="main2">
-        <h2>FINDING THE BEST CLEANSER FOR ACNE</h2>
-<p>
-    An acne cleanser is a type of facial wash that is specially designed for acne-prone skin to help prevent breakouts.
-     An acne cleanser is significantly different than a typical face wash or bar of soap, primarily due to the unique bacteria-fighting ingredients that it contains.
-      By choosing the right face cleanser for your skin, you can get rid of the bacteria that’s clogging hair follicles and causing breakouts. 
-      With this understanding of what a facial cleanser is, we can now cover what acne is and common causes.
-      The best way to treat acne depends on the type and severity of the acne. Acne forms as a result of greasy secretions from the skin’s sebaceous glands (oil glands) that clog the
-       tiny openings of the surface of the skin in the hair follicles, known as pores
-
-</p>
-    </div>
+<?php
+        
+        $blogs=new BlogController;
+        $all=$blogs->readBlog();
+            echo '
+            <div class="main2">
+            <h2>'.$all[1]['title'].'</h2>
+            <p>'.$all[1]['body'].'</p>
+            </div>
+              '
+        ;
+        ?>
 </selection>
 
 <!------------------------------------------------------------------------------------------->
 <selection class="blog3" id="blog3">
-    <div class="main3">
-        <h3>WHAT IS RETINOL? BREAKING DOWN THE KEY BENEFITS</h3>
-<p>
-    Retinol is a commonly used ingredient, but many people still do not know exactly what retinol is.
-     At its base level, retinol is a form of Vitamin A. When placed on the skin's surface, it is converted into retinoic acid. 
-     This skincare ingredient is available in a wide variety of over-the-counter products. When looking at various products containing retinol,
-      you may also run across the term retinoids. To understand which form and potency of retinoic acid are in the skincare product you are using and its associated potency,
-       you must first understand the difference between retinol and retinoids.
-
-</p>
-    </div>
+<?php
+        
+        $blogs=new BlogController;
+        $all=$blogs->readBlog();
+            echo '
+            <div class="main3">
+            <h2>'.$all[2]['title'].'</h2>
+            <p>'.$all[2]['body'].'</p>
+            </div>
+              '
+        ;
+        ?>
 </selection>
 
-
-
-<!------------------------------------------------------------------------------------------->
-
-
 <!-- footer   -->
-
-<section class="footer">
-
-    <div class="box-container">
-
-        <div class="box">
-            <h3> Skincare <i class="fas fa-shopping-basket"></i> </h3>
-            <p>Keep in touch with us!</p>
-            <div class="share">
-                <a href="https://www.facebook.com/" class="fab fa-facebook-f"></a>
-                <a href="https://twitter.com/" class="fab fa-twitter"></a>
-                <a href="https://www.instagram.com/" class="fab fa-instagram"></a>
-                <a href="https://www.linkedin.com/" class="fab fa-linkedin"></a>
-            </div>
-        </div>
-
-        <div class="box">
-            <h3>contact info</h3>
-            <a  class="links"> <i class="fas fa-phone"></i> +38344111222 </a>
-            <a  class="links"> <i class="fas fa-phone"></i> +38349111222 </a>
-            <a  class="links"> <i class="fas fa-envelope"></i> skincare@gmail.com </a>
-            <a  class="links"> <i class="fas fa-map-marker-alt"></i> Prishtinë , Kosovë </a>
-        </div>
-
-        <div class="box">
-            <h3>quick links</h3>
-            <a href="index.html" class="links"> <i class="fas fa-arrow-right"></i> home </a>
-            <a href="file:///C:/Users/rines/Desktop/Projekti%20web/index.html#:~:text=Read%20More-,Our,Features,-Organic%20Ingredients" class="links"> <i class="fas fa-arrow-right"></i> features </a>
-            <a href="products.html" class="links"> <i class="fas fa-arrow-right"></i> products </a>
-            <a href="file:///C:/Users/rines/Desktop/Projekti%20web/index.html#:~:text=Product,Categories" class="links"> <i class="fas fa-arrow-right"></i> categories </a>
-            <a href="blog1.html" class="links"> <i class="fas fa-arrow-right"></i> blogs </a>
-        </div>
-
-
-        <div class="box">
-            <h3>newsletter</h3>
-            <p>subscribe for latest updates</p>
-            <input type="email" placeholder="your email" class="email">
-            <input type="submit" value="subscribe" class="btn">
-            <img src="image/payment.png" class="payment-img" alt="">
-        </div>
-
-    </div>
-
-    <div class="credit"> created by <span> Adi Fejza & Rinesa Mehana </span> | All rights reserved </div>
-
-</section>
-
-<!-- footer section ends -->
-<script src="script.js">    </script>
-
-
-        
+<?php include "includes/footer.php";?>
+<!-- footer -->
     </body>
-
+<script src="js/script.js"></script>
     </html>
