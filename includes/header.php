5b61c348942869d17694550f3446b157e54a0bfa
AdiFejza <93136623+AdiFejza@users.noreply.github.com>
2022-04-24 21:08:11 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,6 @@
+<?php
+session_start()
+?>
 <header class="header">
 
 <a href="index.php" class="logo"> <i class=""></i> Skincare </a>
@@ -22,14 +25,34 @@
     <input type="search" id="search-box" placeholder="search here...">
     <label for="search-box" class="fas fa-search"></label>
 </form>
+<div class="login-form">
+<?php 
+    if(isset($_SESSION['roli'])){ ?>
+        <h3>Welcome <?php echo $_SESSION['emri']; ?></h3>
+        <?php if($_SESSION['roli']==2){?>
+            <a class="btn" href="views/menuDashboard.php">Dashboard</a><?php
+        }?>
+        <a class="btn" href="includes/logout.php">Log Out</a>
+    <?php } 
+    else { ?>
+        <form action="validimi.php" method="POST">
+        <h3>login now</h3>
+        <input name="email" type="email" placeholder="your email" id="emailSign" class="box">
+        <input name="password" type="password" placeholder="your password" id="passSign" class="box">
+        <p>don't have an account <a href="signup.php">create now</a></p>
+        <input name = "submit" type="submit" value="login now" class="btn">
+    </form>
+    <?php    }
+?>
+</div>
 
-<form action="validimi.php" method="POST" class="login-form" >
-    <h3>login now</h3>
-    <input name="email" type="email" placeholder="your email" id="emailSign" class="box">
-    <input name="password" type="password" placeholder="your password" id="passSign" class="box">
-    <p>don't have an account <a href="signup.php">create now</a></p>
-    <input name = "submit" type="submit" value="login now" class="btn">
-</form>
-
+<!-- <form action="validimi.php" method="POST" class="login-form" >
+        <h3>login now</h3>
+        <input name="email" type="email" placeholder="your email" id="emailSign" class="box">
+        <input name="password" type="password" placeholder="your password" id="passSign" class="box">
+        <p>don't have an account <a href="signup.php">create now</a></p>
+        <input name = "submit" type="submit" value="login now" class="btn">
+        <p></p>
+    </form> -->
 
 </header>
